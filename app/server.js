9cd3edac0a6282e2ac95708cd0496cbb9be98d0e
Ningaanza Remy <remimaradona@gmail.com>
2021-12-25 20:36:57 -0700

    create folders structure of our app, and a simple About route

--- app/server.js
+++ app/server.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("Welcome to my Blog!");
+});
+app.get("/about", (req, res) => {
+	res.send("Go to about page");
 });
 
 app.listen(port, () => {
