989462f80ffb40e0e61f02c925be87699786a6e0
Ningaanza Remy <remimaradona@gmail.com>
2021-12-26 22:54:34 -0700

    installed pug template engine and tested and renderd pug template

--- app/routes.js
+++ app/routes.js
@@ -2,27 +2,36 @@ const express = require("express"),
 	router = express.Router();
 
 router.get("/", (req, res) => {
-	res.send("Welcome to my Blog!");
+	return res.render("../assets/views/homepage.pug");
 });
-router.get("/about", (req, res) => {
-	res.send("Go to about page");
-});
-router.get("/user/:username/:state/", (req, res) => {
-	console.log(req.params);
+//router.get("/about", (req, res) => {
+//	res.send("Go to about page");
+//});
+////router.get("/user/:username/:state/", (req, res) => {
+//	console.log(req.params);
 
-	const user = req.params;
-	const query = req.query;
-	const car = req.query;
+//	const user = req.params;
+//	const query = req.query;
+//	const car = req.query;
 
-	res.send(` 
-	
-	<h1> The user is: ${user.username} </h1>
-	<h1>Home State: ${user.state}</h1>
-	<h1> Low is: ${query.low} </h1>
-	<h1>High is: ${query.high} </h1>
-	<h1>Car is: ${query.car} </h1>
+//	res.send(`
 
-	`);
+//	<h1> The user is: ${user.username} </h1>
+//	<h1>Home State: ${user.state}</h1>
+//	<h1> Low is: ${query.low} </h1>
+//	<h1>High is: ${query.high} </h1>
+//	<h1>Car is: ${query.car} </h1>
+
+//	`);
+//});
+router.get("./pug", (req, res) => {
+	return res.send("../assets/views/testing.pug", {
+		username: "Remix",
+		fname: "coder",
+		lname: "javascript",
+		loggedIn: true,
+		friends: ["Jane", "Johny", "Jill", "Julia"],
+	});
 });
 
 module.exports = router;
--- app/server.js
+++ app/server.js
@@ -4,8 +4,16 @@ const port = 3000;
 
 const routes = require("./routes.js");
 
+//Routes
 app.use("/", routes);
 
+//static files folder
+app.use(express.static("public"));
+
+//Template Engines
+app.set("view engine", "pug");
+
+//listening on port 3000
 app.listen(port, () => {
 	console.log(`postpage app listening at http://localhost:${port}`);
 });
