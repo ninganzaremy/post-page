d34f4916727abefe2a35a33ac510a5ac83b883be
Ningaanza Remy <remimaradona@gmail.com>
2022-01-03 11:31:28 -0700

    posts route created, tested the post and saved it in mysql

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 const express = require("express"),
 	router = express.Router();
+const DB = require("../database/connection");
 
 router.get("/", (req, res) => res.render("../assets/views/homepage.pug"));
 
@@ -7,26 +8,45 @@ router.get("/generic", (req, res) => res.render("../assets/views/generic.pug"));
 
 router.get("/postCreate", (req, res) => res.render("../assets/views/postCreate.pug"));
 
-//router.get("/about", (req, res) => {
-//	res.send("Go to about page");
-//});
-////router.get("/user/:username/:state/", (req, res) => {
-//	console.log(req.params);
+router.post("/postCreate", (req, res) => {
+	const post = req.body;
+	DB.query(
+		`INSERT INTO posts (title, description, image_url) VALUES('${post.title}', '${post.description}', '${post.image_url}')`,
 
-//	const user = req.params;
-//	const query = req.query;
-//	const car = req.query;
+		(error, res) => {
+			if (error) {
+				console.log("error:");
+				console.log(error);
+				return res.redirect("/postCreate");
+			} else {
+				return res.redirect("/");
+			}
+		},
+	);
+	//return res.json(post);
+	//return res.render("../assets/views/postCreate.pug");
+	//return res.render("../assets/views/postCreate.pug");
+});
+router.get("/about", (req, res) => {
+	res.send("Go to about page");
+});
+router.get("/user/:username/:state/", (req, res) => {
+	console.log(req.params);
 
-//	res.send(`
+	const user = req.params;
+	const query = req.query;
+	const car = req.query;
 
-//	<h1> The user is: ${user.username} </h1>
-//	<h1>Home State: ${user.state}</h1>
-//	<h1> Low is: ${query.low} </h1>
-//	<h1>High is: ${query.high} </h1>
-//	<h1>Car is: ${query.car} </h1>
+	res.send(`
 
-//	`);
-//});
+	<h1> The user is: ${user.username} </h1>
+	<h1>Home State: ${user.state}</h1>
+	<h1> Low is: ${query.low} </h1>
+	<h1>High is: ${query.high} </h1>
+	<h1>Car is: ${query.car} </h1>
+
+	`);
+});
 router.get("/pug", (req, res) => {
 	return res.render("../assets/views/testing.pug", {
 		username: "Remix",
--- app/server.js
+++ app/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const DB = require("../database/connection");
-
+const bodyParser = require("body-parser");
 const port = 3000;
 
 const routes = require("./routes.js");
+
+//using BodyParser for post request data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Routes
 app.use("/", routes);
