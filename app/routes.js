060c26980c70b07af3f86234b44d074d319c8d42
Ningaanza Remy <remimaradona@gmail.com>
2022-01-07 21:44:12 -0700

    created editPost route, edited the post and updated the post in DB

--- app/routes.js
+++ app/routes.js
@@ -22,9 +22,6 @@ router.post("/post/create", (req, res) => {
 			}
 		},
 	);
-	//return res.json(post);
-	//return res.render("../assets/views/postCreate.pug");
-	//return res.render("../assets/views/postCreate.pug");
 });
 
 router.get("/post/:id", (req, res) => {
@@ -43,6 +40,46 @@ router.get("/post/:id", (req, res) => {
 		},
 	);
 });
+
+router.get("/post/:id/edit", (req, res) => {
+	DB.query(
+		`SELECT * FROM posts WHERE id = ${req.params.id} LIMIT 1`,
+
+		(error, result) => {
+			if (error) {
+				console.log("error:");
+				console.log(error);
+				return res.redirect("/");
+			} else {
+				console.log(result[0]);
+				return res.render("../assets/views/post/edit.pug", {
+					id: req.params.id,
+					title: result[0].title,
+					description: result[0].description,
+					image_url: result[0].image_url,
+				});
+			}
+		},
+	);
+});
+
+router.post("/post/:id/edit", (req, res) => {
+	const post = req.body;
+	DB.query(
+		`UPDATE posts SET title = '${post.title}', description = '${post.description}', image_url= ${post.image_url}') `,
+
+		(error, result) => {
+			if (error) {
+				console.log("error:");
+				console.log(error);
+				return res.redirect("/post/create");
+			} else {
+				return res.redirect(`/post/${req.params.id}/edit`);
+			}
+		},
+	);
+});
+
 //router.get("/about", (req, res) => {
 //	res.send("Go to about page");
 //});
